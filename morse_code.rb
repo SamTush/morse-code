bad5bb8ce8d10e38019d1b3581fefb78a92007e7
Samson Tush <samo4421@icloud.com>
2023-05-18 15:01:00 +0300

    Fix linter errors

--- morse_code.rb
+++ morse_code.rb
@@ -40,9 +40,58 @@ end
 def decode_message(message)
   words = message.split('   ').map { |word| decode_word(word) }
   decoded_message = words.join(' ')
-  return decoded_message
+  decoded_message
 end
-  
+
 decode_char(".-")
 puts decode_word("-- -.--")
 puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+$morse_code = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(string)
+  puts $morse_code[string]
+end
+
+def decode_word(morse)
+  decoded_chars = morse.split.map { |char| $morse_code[char] }
+  decoded_word = decoded_chars.join('')
+  decoded_word
+end
+
+def decode_message(message)
+  words = message.split('   ').map { |word| decode_word(word) }
+  decoded_message = words.join(' ')
+  decoded_message
+end
+
+decode_char(".-")
+puts decode_word("-- -.--")
+puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+  
